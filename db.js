6ff286dce3815dbcd264a7aee1deb82ddac9e049
Roman Podobny <tazer16179@gmail.com>
2021-06-28 19:18:40 +0300

    db commit

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const config = require("config")
  const nameDb = config.get('Parametrs.nameDb'); 
 const sequelize = new Sequelize( nameDb, userName, dbPassword, {
     host: host,
-    port:8889,
+    port:3306,
     dialect: "mysql",
   });
 module.exports = sequelize;
